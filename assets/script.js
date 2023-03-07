ef368d74aeba465ffcadffe0ecc19b3188a97807
Marshall <marshall.peters01@gmail.com>
2023-03-06 21:33:50 -0500

    lots of clues and more work!

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,24 @@
-var choices = ["Rock", "Paper", "Scissors"]
-var stats = ["Wins", "Losses", "Ties"]
+var wins = 0;
+var ties = 0;
+var losses = 0;
+var choices = ["Rock", "Paper", "Scissors", "Shotgun"]
 
-function playGame(){
-var selected = window.prompt("Enter rock, paper or scissors.");
+function playGame()
+var selected = window.prompt("Enter rock, paper or scissors.(Not shotgun. Definitely do NOT choose shotgun.)");
 selected.toUpperCase
-var random = Math.floor(Math.random() * choices.length);
-
+var random = Math.floor(Math.random() * 3);
+if (selected = shotgun){
+    window.alert("DUDE WTF YOU BROUGHT A SHOTGUN TO A ROCK PAPER SCISSORS GAME??!?!!?!")
+    return;
+    
+} else {
 window.alert("You selected " + selected + ". The computer selected " + random)
+};
 
-}
+function playAgain(){
+
+};
 
 playGame();
+
+playAgain();
